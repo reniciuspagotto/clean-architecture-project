c736bb39b696941869a3d681b5d8e25a4c6fed59
Renícius Pagotto Fostaini <renicius.pagotto@outlook.com>
2018-04-02 20:34:20 -0300

    Adicionado projeto de teste com xunit

--- ArquiteturaPadrao.Test/UnitTest1.cs
+++ ArquiteturaPadrao.Test/UnitTest1.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
 
-namespace ArquiteturaPadrao.AppTest
+namespace ArquiteturaPadrao.Test
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+
         }
     }
 }
